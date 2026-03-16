7a898f9faf926c3376b730510ba236f970f74667
WALHAY <walhayggwp@gmail.com>
2026-03-16 14:44:00 +0300

    Docker compose

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -1,5 +1,10 @@
+{ pkgs, ... }:
 {
-	virtualisation.docker = {
-		enable = true;
-	};
+  environment.systemPackages = with pkgs; [
+    docker-compose
+  ];
+
+  virtualisation.docker = {
+    enable = true;
+  };
 }
